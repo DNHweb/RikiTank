56968b158b313122ff15426fde045dd34f044950
debuss <zizilex@gmail.com>
2012-10-22 21:42:50 +0200

    maj

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(a)
 
 	a.title = "RikiTank"
 	a.screen.fullscreen = true
-	a.screen.width = 1024
-    	a.screen.height = 768
+	--a.screen.width = 1024
+    	--a.screen.height = 768
 
 end
