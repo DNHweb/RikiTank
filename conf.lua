045f46e9711230c141d451be6906e6b326ad2eba
Riki-Al3xis <n.al3xis@gmail.com>
2012-10-22 22:33:43 +0300

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -3,14 +3,6 @@ function love.conf(a)
 	a.title = "RikiTank"
 	a.screen.fullscreen = true
 	a.screen.width = 1024
-    a.screen.height = 768
-	
+    	a.screen.height = 768
 
-
-	--> ESCAPE GAME <--
-	-- function love.keypressed(key)
-		-- if key == "escape" then
-			-- love.event.push("quit")
-		-- end
-	-- end
 end
