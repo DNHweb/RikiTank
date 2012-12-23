3cdac112f431162816bf53a400f0b237ae368161
debuss <zizilex@gmail.com>
2012-12-23 17:36:27 +0100

    Bug Fix : Missile.lua et Walker.lua
    Renommage de ("base") en ("Base") pour eviter l'erreur de nom de fichier
    sur UNIX.

--- Entities/Missile.lua
+++ Entities/Missile.lua
@@ -17,7 +17,7 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -- 
 
-local ent = ents.Derive("base")
+local ent = ents.Derive("Base")
 
 function ent:setPos( x, y )
 	self.x = x
